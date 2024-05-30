e38bc602612cc21b8dc501dd346a5e23854a1935
magalhaesArtur <joaoartur4590@gmail.com>
2024-05-29 23:57:02 -0300

    feat: innitial commit

---
+++ src/test/java/com/desafioInsightLab/DesafioInsightLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioInsightLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioInsightLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
