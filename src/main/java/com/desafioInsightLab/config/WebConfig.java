1da453495ee5616ccb134712fd29451c5bd0520e
magalhaesArtur <joaoartur4590@gmail.com>
2024-06-03 18:17:35 -0300

    config: add dockerfile

--- src/main/java/com/desafioInsightLab/config/WebConfig.java
+++ src/main/java/com/desafioInsightLab/config/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("http://localhost:3000","http://localhost:8080")
+                        .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
