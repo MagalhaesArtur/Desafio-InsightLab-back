005b08b650c1113b0288961c3ddbbf8be4fb8859
magalhaesArtur <joaoartur4590@gmail.com>
2024-06-04 15:56:01 -0300

    config: email is now use to login

--- src/main/java/com/desafioInsightLab/config/WebConfig.java
+++ src/main/java/com/desafioInsightLab/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","http://localhost:8080")
+                        .allowedOrigins("http://localhost:3000","http://localhost:8080","https://desafio-insight-lab-front.vercel.app")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
